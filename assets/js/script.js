0d2dbf4a2bf9ef08b34ad24d68259d4b5c500725
Carla Latorre <clatorresavalli@gmail.com>
2022-11-19 22:57:03 -0300

    Incorporación de JS

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+//NavBar/
+
+$(document).ready(function(){
+
+    $("a").click(function(){
+        var gato= this.hash
+    
+        $("html, body"). animate(
+            {
+                scrollTop: $(gato).offset().top -77
+            },
+            800
+        )
+    })
+    
+    })
+    
+    
+    var nav = document.querySelector("nav");
+    window.addEventListener("scroll", function() {
+        if (this.window.pageYOffset >100) {
+            nav.classList.add("bg-dark");
+        } else {
+            nav.classList.remove("bg-dark");
+        }
+    });    
+        
+    
+    
+    
+    //Tooltip botón enviar//
+    
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+    
+    //Alerta mensaje enviado correctamente//
+    
+    $("#btn-enviar").click(function(){
+        alert("Tu mensaje fue enviado correctamente")
+        });
+    
+    
+    
